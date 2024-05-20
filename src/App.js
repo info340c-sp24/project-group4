654a25c00418365e6cc9f047cf6b406247d71279
anicana <anicana@uw.edu>
2024-05-19 18:26:08 -0700

    edits to quizzes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import MainPage from './pages/MainPage';
 import Footer from './components/Footer';
 import WordPage from './pages/WordPage';
+import Quizzes from './pages/Quizzes'
 
 function App() {
   return (
