ae24be01b9abfc1cc7bc6b75528dd961702426df
alexhan04 <alexjaehyunhan@gmail.com>
2024-06-03 03:14:31 -0700

    added mobile navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import '../index.css';
-
 function Header() {
   const [showMobileNav, setShowMobileNav] = useState(false);
 
